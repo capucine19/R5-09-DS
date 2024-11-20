acd30bd12ee4ffdaeba3ed9bd46e967ea7608f34
Capucine <44448753+capucine19@users.noreply.github.com>
2024-11-20 14:23:02 +0100

    Update Q5.java

--- Q5.java
+++ Q5.java
@@ -3,7 +3,7 @@ public class Q5 {
         if (args.length > 0) {
             try {
                 int n = Integer.parseInt(args[0]);
-                for (int i = 1; i <= n; i++) {
+                for (int i = n; i >= 1; i--) { // Boucle inversée
                     for (int j = 0; j < i; j++) {
                         System.out.print("*");
                     }
